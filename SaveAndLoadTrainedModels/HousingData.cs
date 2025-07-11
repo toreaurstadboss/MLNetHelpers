94490e18034738d5e85a444246a06d5b719df9f9
Tore Aurstad <tore.aurstad@gmail.com>
2025-07-12 00:19:37 +0200

    added save ml.net sample console app project for testing out saving ml.net model

---
+++ SaveAndLoadTrainedModels/HousingData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ML.Data;
+
+namespace SaveAndLoadTrainedModels
+{
+
+    public class HousingData
+    {
+
+        public float Size { get; set; }
+
+        [VectorType(3)]
+        public float[] HistoricalPrices { get; set; } = Array.Empty<float>();
+
+        [ColumnName("Label")]
+        public float CurrentPrice { get; set; }
+
+    }
+
+}
---
+++ SaveAndLoadTrainedModels/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+
+namespace SaveAndLoadTrainedModels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HousingData[] housingData = new HousingData[]
+            {
+                new HousingData
+                {
+                    Size = 600f,
+                    HistoricalPrices = new float[] { 100000f, 125000f, 122000f },
+                    CurrentPrice = 170000f
+                },
+                new HousingData
+                {
+                    Size = 1000f,
+                    HistoricalPrices = new float[] { 200000f, 250000f, 230000f },
+                    CurrentPrice = 225000f
+                },
+                new HousingData
+                {
+                    Size = 1000f,
+                    HistoricalPrices = new float[] { 126000f, 130000f, 200000f },
+                    CurrentPrice = 195000f
+                }
+
+            };
+
+            //Create MLContext 
+            MLContext mlContext = new MLContext();
+
+            // Load Data 
+            IDataView data = mlContext.Data.LoadFromEnumerable(housingData);
+
+            // Define data preparation estimator 
+            EstimatorChain<RegressionPredictionTransformer<LinearRegressionModelParameters>> pipelineEstimator =
+                mlContext.Transforms            
+                .Concatenate("Features", new string[] { "Size", "HistoricalPrices" })
+                .Append(mlContext.Transforms.NormalizeMinMax("Features"))
+                .Append(mlContext.Regression.Trainers.Sdca());
+
+            // train model - regression model
+            ITransformer trainedModel = pipelineEstimator.Fit(data);
+
+            // Save model
+            mlContext.Model.Save(trainedModel, data.Schema, "model.zip");
+
+            Console.WriteLine($"Saved ML.Net model to output directory: {Path.Combine(Environment.CurrentDirectory, "model.zip")}");
+
+        }
+
+    }
+}
