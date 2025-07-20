0a2330e6f85f433a6805664a1605e581a730d627
Tore Aurstad <tore.aurstad@gmail.com>
2025-07-20 21:24:30 +0200

    update

--- StopSignDetection_WebApi1/Program.cs
+++ StopSignDetection_WebApi1/Program.cs
@@ -28,7 +28,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Object detection - Machine learning MLN.");
     });
 }
 
